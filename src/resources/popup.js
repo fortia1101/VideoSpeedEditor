468c4a2e6818cc6451eed4e33a5d2f9e741fd6fc
ritto <masa00.vr3@gmail.com>
2023-10-29 20:37:58 +0900

    Create VSE v1.0.0

---
+++ src/resources/popup.js
@@ -0,0 +1,56 @@
+// popup.html Loaded
+window.addEventListener("DOMContentLoaded", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting
+        .executeScript({
+            target: { tabId: tab.id },
+            func: getNowPlaySpeed,
+        })
+        .then((injectionResults) => {
+            for (const { frameId, result } of injectionResults) {
+                if (result[0] === 0) {
+                    document.querySelector(".video-active").style.display = "none";
+                    document.querySelector(".video-deactive").style.display = "block";
+                } else {
+                    document.querySelector(".video-active").style.display = "block";
+                    document.querySelector(".video-deactive").style.display = "none";
+                }
+                document.getElementById("speed-select").querySelector(`option[value='${result[1]}']`).selected = true;
+            }
+        });
+});
+
+let speedSelector = document.getElementById("speed-select");
+speedSelector.addEventListener("change", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: switchPlaySpeed,
+        args: [tab, speedSelector.value],
+    });
+});
+
+// Handle Current Page
+function getNowPlaySpeed() {
+    let videoElements = document.getElementsByTagName("video");
+    let nowSpeed = 1.0;
+    try {
+        nowSpeed = videoElements[0].playbackRate;
+    } catch {
+        nowSpeed = 1.0;
+    }
+    for (let i = 1; i < videoElements.length; i++) {
+        videoElements[i].playbackRate = nowSpeed;
+    }
+    return [videoElements.length, nowSpeed];
+}
+
+// Handle Current Page
+function switchPlaySpeed(tab, speed) {
+    let videoElements = document.getElementsByTagName("video");
+    for (let i = 0; i < videoElements.length; i++) {
+        videoElements[i].playbackRate = speed;
+    }
+    console.log(`Switched to ${speed}s`);
+}
